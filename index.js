bea9d06f5193ab8434e62fa6171abaaa159a898b
megarohas <a.galkin@desoft.pro>
2022-08-18 12:17:27 +0600

    express was added

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
-const http = require("http");
-
-const host_name = process.env.HOST_NAME || "127.0.0.1";
+let express = require("express");
+let app = express();
+const host_name = process.env.HOST_NAME || "localhost";
 const port = 9000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "Hello World =)" }));
-});
+app.listen(port);
 
-server.listen(port, host_name, () => {
-  console.log(`Server running at http://${host_name}:${port}/`);
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World =)" });
 });
